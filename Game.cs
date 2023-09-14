6ae23fcb384fed4d0f661c0fbb850d808b7ff7ec
David Tran <109535790+david125tran@users.noreply.github.com>
2023-09-13 20:27:14 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+public class Game
+{
+    // ------------------------ Public Member Variables ------------------------
+    public List<char> _field = new List<char>();
+
+    // ------------------------ Public Properties ------------------------
+
+
+    // ------------------------ Default Constructor ------------------------
+    /// <summary>This method intializes the TicTacToe board when the Game() class is called.</summary>
+    public Game()
+    {
+        _field.Add('1');
+        _field.Add('2');
+        _field.Add('3');
+        _field.Add('4');
+        _field.Add('5');
+        _field.Add('6');
+        _field.Add('7');
+        _field.Add('8');
+        _field.Add('9');
+
+    }
+
+    // ------------------------ Member Methods ------------------------
+    /// <summary>This method displays the TicTacToe board.</summary>
+    public void ShowField()
+    {
+        Console.WriteLine("\n");
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" {0} | {1} | {2} ", _field[0], _field[1], _field[2]);
+        Console.WriteLine("___|___|___");
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" {0} | {1} | {2} ", _field[3], _field[4], _field[5]);
+        Console.WriteLine("___|___|___");
+        Console.WriteLine("   |   |   ");
+        Console.WriteLine(" {0} | {1} | {2} ", _field[6], _field[7], _field[8]);
+        Console.WriteLine("   |   |   ");
+    }
+
+    /// <summary>This method retrieves what is in a square.</summary>
+    /// <returns>what is in the square of the TicTacToe board.</returns>
+    public char GetSquare(int choice)
+    {
+        return _field[choice];
+    }
+
+    /// <summary>This method updates the TicTacToe board with the player's mark</summary>
+    public void UpdateSquare(int choice, int player)
+    {
+        if (player == 1)
+        {
+            _field[choice - 1] = 'X';
+        }
+        else
+        {
+            _field[choice - 1] = 'O';
+        }
+    }
+
+    /// <summary>This method checks for a winner or draw.</summary>
+    /// <returns>true for if there is a win or draw.  false for if there is not a win or draw</returns>
+    public bool CheckForWinner()
+    {
+        // ----------------------- Check for a winner in rows -----------------------
+        if (_field[0] == _field[1] && _field[0] == _field[2])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[0]);
+            return true;
+        }
+        else if (_field[3] == _field[4] && _field[3] == _field[5])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[3]);
+            return true;
+        }
+        else if (_field[6] == _field[7] && _field[6] == _field[8])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[6]);
+            return true;
+        }
+
+        // ----------------------- Check for a winner in columns -----------------------
+        else if (_field[0] == _field[3] && _field[0] == _field[6])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[0]);
+            return true;
+        }
+        else if (_field[1] == _field[4] && _field[1] == _field[7])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[1]);
+            return true;
+        }
+        else if (_field[2] == _field[5] && _field[2] == _field[8])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[2]);
+            return true;
+        }
+
+        // ----------------------- Check for a winner in diagonals -----------------------
+        else if (_field[0] == _field[4] && _field[0] == _field[8])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[0]);
+            return true;
+        }
+        else if (_field[2] == _field[4] && _field[2] == _field[6])
+        {
+            Console.WriteLine("\n\nPlayer {0} wins!", _field[2]);
+            return true;
+        }
+
+        // ----------------------- Check if field is full -----------------------
+        else if (_field[0] != '1' && _field[1] != '2' && _field[2] != '3' && _field[3] != '4' && _field[4] != '5' && _field[5] != '6' && _field[6] != '7' && _field[7] != '8' && _field[8] != '9')
+        {
+            Console.WriteLine("\n\nThere is not winner!");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    /// <summary>This method resets the TicTacToe field to a new game.</summary>    
+    public void ResetField()
+    {
+        _field[0] = '1';
+        _field[1] = '2';
+        _field[2] = '3';
+        _field[3] = '4';
+        _field[4] = '5';
+        _field[5] = '6';
+        _field[6] = '7';
+        _field[7] = '8';
+        _field[8] = '9';
+
+    }
+
+}
---
+++ Project.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeOOP
+
+{
+    public class Project
+    {
+        static void Main(string[] args)
+        {
+            // ----------------------- Declare the initial player -----------------------
+            int player = 1;
+            int choice;
+
+            // ----------------------- Create a new object of the Game() class -----------------------
+            Game myGame = new Game();
+            
+            while (true)
+            {
+                // ----------------------- Display the field -----------------------
+                myGame.ShowField();
+                Console.WriteLine("\nPlayer 1 = X          Player 2 = O");
+
+                while (true)
+                {
+                    // ----------------------- Tell player to make a choice -----------------------
+                    Console.WriteLine("Player {0}, please type your choice:", player);
+
+                    // ----------------------- Handle cases where player makes a choice on the board -----------------------
+                    try
+                    {
+                        choice = int.Parse(Console.ReadLine());
+
+                        if (choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6 || choice == 7 || choice == 8 || choice == 9)
+                        {
+                            // ----------------------- Handle cases where player chooses a spot already taken -----------------------
+                            if (choice == 1 && myGame.GetSquare(choice - 1) != '1')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 2 && myGame.GetSquare(choice - 1) != '2')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 3 && myGame.GetSquare(choice - 1) != '3')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 4 && myGame.GetSquare(choice - 1) != '4')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 5 && myGame.GetSquare(choice - 1) != '5')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 6 && myGame.GetSquare(choice - 1) != '6')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 7 && myGame.GetSquare(choice - 1) != '7')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 8 && myGame.GetSquare(choice - 1) != '8')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            else if (choice == 9 && myGame.GetSquare(choice -1 ) != '9')
+                            {
+                                Console.WriteLine("Please make a valid selection that is not already marked");
+                            }
+                            // ----------------------- Handle cases where player chooses a valid spot -----------------------
+                            else
+                            {
+                                break;  // Break out of the loop and start marking the board
+                            }
+                        }
+                    }
+
+                    // ----------------------- Handle cases where player did not make a valid choice -----------------------
+                    catch
+                    {
+                        Console.WriteLine("\nPlease make a valid selection");   // Stay in the while loop until a valid choice is made
+                    }
+                }
+
+                // ----------------------- Mark the board -----------------------
+                myGame.UpdateSquare(choice, player);
+
+                // ----------------------- Check for a winner -----------------------
+                if (myGame.CheckForWinner())
+                {
+                    myGame.ShowField();
+                    Console.WriteLine("\nLet's play again?");
+                    myGame.ResetField();
+                }    
+
+                // ----------------------- Switch Players -----------------------
+                if (player == 1)
+                {
+                    player = 2;
+                }
+                else
+                {
+                    player = 1;
+                }
+            }
+        }
+    }
+}
